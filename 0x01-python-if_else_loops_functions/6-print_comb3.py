940fe47b3006be5ed2fa2a97be4089c5690eafed
EstherKiguru <esipraisewambui@gmail.com>
2023-11-29 21:53:13 +0300

    brains and materials.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,5 +5,4 @@ for num in range(10):
         if num == 8 and i == 9:
             print("{:d}{:d}".format(num, i))
         else:
-             print("{:d}{:d}".format(num, i), end=", ")
-            
+            print("{:d}{:d}".format(num, i), end=", ")
