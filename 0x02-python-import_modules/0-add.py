99dfd83e5d3914f046bbfc6b7465ef7b10b3a498
EstherKiguru <esipraisewambui@gmail.com>
2023-12-06 10:58:05 +0300

    simple function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-solution = add(a, b)
-print("{} + {} = {}".format(a, b, solution))
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    solution = add(a, b)
+    print("{} + {} = {}".format(a, b, solution))
