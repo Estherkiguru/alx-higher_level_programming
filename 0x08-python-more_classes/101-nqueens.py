9b2bca7e4afc54de8865ba8bdd96eff8f5b6fe4d
EstherKiguru <esipraisewambui@gmail.com>
2024-01-05 22:18:57 +0300

    N queens

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import sys
+
+
+def is_safe(board, row, col, N):
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+
+def solve_nqueens(board, row, N, solutions):
+    if row == N:
+        solutions.append([[i, board[i]] for i in range(N)])
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row] = col
+            solve_nqueens(board, row + 1, N, solutions)
+
+
+def print_solutions(N):
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(N)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * N
+    solutions = []
+    solve_nqueens(board, 0, N, solutions)
+
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./101-nqueens.py N")
+        sys.exit(1)
+
+    print_solutions(sys.argv[1])
