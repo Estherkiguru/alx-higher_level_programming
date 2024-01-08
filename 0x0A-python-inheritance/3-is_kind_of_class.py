a50d1773fef08560bf440713bced1d65c36a5ccc
EstherKiguru <esipraisewambui@gmail.com>
2024-01-09 00:06:35 +0300

    same or inherent

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module for same class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if obj is an instance 0f or inherits from a_class"""
+    return isinstance(obj, a_class)
