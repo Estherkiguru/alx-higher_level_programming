0094145c374dbb1933521d910e606340e40b2bb4
EstherKiguru <esipraisewambui@gmail.com>
2024-01-09 00:22:13 +0300

    only subclass of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+"""module for only sub class of"""
+
+
+def inherits_from(obj, a_class):
+    """checks if an object is a true subclass"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
