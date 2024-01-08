42376e88613dc32a2332077cc2fbe61845f654ec
EstherKiguru <esipraisewambui@gmail.com>
2024-01-09 00:47:06 +0300

    exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+"""module for the improbe basegeometry"""
+
+
+class BaseGeometry:
+    """baseGeometry class"""
+    def area(self):
+        """raises an exemption"""
+        raise Exception("area() is not implemented")
