d28b675260829ce71fafbacb74ce7faa206d0f21
EstherKiguru <esipraisewambui@gmail.com>
2024-01-08 22:41:36 +0300

    1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""defines the attributes of the object lookup function"""
 
 
 def lookup(list_obj):
     """Returns a list object"""
-    return dir(list_obj)
+    return (dir(list_obj))
