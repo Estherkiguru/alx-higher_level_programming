b5a05bea34cb45f5db356cd1ff0293a6276c84c2
EstherKiguru <esipraisewambui@gmail.com>
2024-01-09 21:20:04 +0300

    JSON

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module for the JSON representation of an object"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the json representation of an object"""
+    return json.dumps(my_obj)
