313f5cf907094598d804ca2db985a28bbc37c511
EstherKiguru <esipraisewambui@gmail.com>
2024-01-10 21:25:54 +0300

    add integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,10 +16,11 @@ def add_integer(a, b=98):
     int: The sum of a and b
     """
     if type(a) not in (int, float):
-            raise TypeError('a must be an integer')
-    if type(b)not in (int, float)):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
         raise TypeError('b must be an integer')
     return int(a) + int(b)
+
 
 if __name__ == "__main__":
     import doctest
