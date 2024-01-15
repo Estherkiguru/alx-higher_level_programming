b31fcae543635d8da756249d4ad88bfd55f5b2fc
EstherKiguru <esipraisewambui@gmail.com>
2024-01-15 23:26:33 +0300

    almost a circle project

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,51 +13,51 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-        @property
-        def width(self):
-            """The rectangle's width"""
-            return self.__width
+    @property
+    def width(self):
+        """The rectangle's width"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            self.validate_integer("width", value, False)
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        self.validate_integer("width", value, False)
+        self.__width = value
 
-        @property
-        def height(self):
-            """The rectangle's height"""
-            return self.__height
+    @property
+    def height(self):
+        """The rectangle's height"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            self.validate_integer("height", value, False)
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        self.validate_integer("height", value, False)
+        self.__height = value
 
-        @property
-        def x(self):
-            """The rectangle's x"""
-            return self.__x
+    @property
+    def x(self):
+        """The rectangle's x"""
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            self.validate_integer("x", value, False)
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        self.validate_integer("x", value, False)
+        self.__x = value
 
-        @property
-        def y(self):
-            """The rectangle's y"""
-            return self.__y
+    @property
+    def y(self):
+        """The rectangle's y"""
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            self.validate_integer("y", value, False)
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        self.validate_integer("y", value, False)
+        self.__y = value
 
-        def validate_integer(self, name, eq=True):
-            """validate the attributes of the rectangle"""
-            if type(value) is not int:
-                raise TypeError("{} must be an integer".format(name))
-            if eq and value < 0:
-                raise ValueError("{} must be >= 0".format(name))
-            elif not q and value <= 0:
-                raise ValueError("{} must be > 0".format(name))
+    def validate_integer(self, name, value, eq=True):
+        """validate the attributes of the rectangle"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
