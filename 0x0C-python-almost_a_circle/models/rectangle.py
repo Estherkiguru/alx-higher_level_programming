c854f9ea6e682ba6539495bc09171720b33598e6
EstherKiguru <esipraisewambui@gmail.com>
2024-01-16 08:33:53 +0300

    almost a circle project

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,7 +74,7 @@ class Rectangle(Base):
         """validate the attributes of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
-        if eq and value <= 0:
+        if eq and value < 0:
             raise ValueError("{} must be > 0".format(name))
-        elif not eq and value < 0:
-            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
