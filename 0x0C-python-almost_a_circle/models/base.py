5808d96d32922ba16a0aaedc36c461066405e687
EstherKiguru <esipraisewambui@gmail.com>
2024-01-17 23:39:37 +0300

    almost a circle project

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Class Base module"""
 import json
+import csv
 
 
 class Base:
@@ -63,3 +64,37 @@ class Base:
             return []
         with open(file, "r", encoding="utf-8") as f:
             return [cls.create(**d) for d in cls.from_json_string(f.read())]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """serializes object to CSV file"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if list_objs is not None:
+            if cls is Rectangle:
+                for obj in list_objs:
+                    list_objs = [obj.id, obj.width, obj.height, obj.x, obj.y]
+            else:
+                for obj in list_objs:
+                    list_objs = [obj.id, obj.size, obj.x, obj.y]
+        with open("{}.csv".format(cls.__name__), 'w', newline='', encoging="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerows(list_objs)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """deserializes list_obj from csv file"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+        loaded_objs = []
+        with open("{}.csv".format(cls.__name__), 'r', newline='', encoding="utf-8") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                row = [int(r) for r in row]
+                if cls is Rectangle:
+                    d = ("id": row[0], "width": row[1], "height":
+                        row[2], "x": row[3], "y": row[4])
+                else:
+                d = ("id": row[0], "size": row[1], "x": row[2], "y": row[3])
+                loaded_objs(cls.create(**d))
+                return loaded_objs
