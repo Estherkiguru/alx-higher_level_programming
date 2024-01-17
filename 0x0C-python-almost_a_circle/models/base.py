1b7762d4e1cfc074549f41b07284b972e0607af9
EstherKiguru <esipraisewambui@gmail.com>
2024-01-17 20:53:05 +0300

    almost a circle project

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,9 +26,9 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """returns the list of the JSON string representation json_string"""
+        """returns the list of the JSON string representation"""
         if json_string is None or not json_string:
-            return "[]"
+            return []
         else:
             return json.loads(json_string)
 
