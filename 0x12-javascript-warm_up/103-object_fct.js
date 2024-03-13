0240be21e888c715bef50e31dbb67fa4997c9e16
EstherKiguru <esipraisewambui@gmail.com>
2024-03-13 07:08:22 -0700

    JavaScript - Warm up

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,13 +2,14 @@
 
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function () {
-    this.value++;
-  }
+  value: 12
 };
 
 console.log(myObject);
+myObject.incr = function incr () {
+  this.value++;
+};
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
