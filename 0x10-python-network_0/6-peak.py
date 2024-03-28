6c52516cbc0016a33251bbb64a4ccb70878c253f
EstherKiguru <esipraisewambui@gmail.com>
2024-03-28 01:31:13 -0700

    python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,7 +2,7 @@
 """ Find a peak in a list of unsorted integers """
 
 def find_peak(list_of_integers):
-
+    """ Function to find a peak in a list of unsorted integers """
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
 
