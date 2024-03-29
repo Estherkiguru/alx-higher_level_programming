f7da4436a1088fb2f66a8e71f0f13187a68260c9
EstherKiguru <esipraisewambui@gmail.com>
2024-03-28 20:44:07 -0700

    python network

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+takes in URL and email, sends POST request, displays response body
+"""
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {"email": email}
+    response = requests.post(url, data=payload)
+    print(response.text)
+    
